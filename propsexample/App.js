04f39d79bbada264b637caab0034d0545f0a898a
Ash Patil <patilak@gmail.com>
2025-06-09 17:14:53 -0700

    adding props example

---
+++ propsexample/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Raj" />
+            <Heading firstName="Kim"/>
+        </div>
+    );
+};
+
+export default App;
